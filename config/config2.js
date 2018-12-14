5fdbf0bf0674f7ea1e4e6d533cdd419e05e47454
Arthur12320 <arthurraragao@hotmail.com>
2018-12-13 22:50:31 -0300

    fixed wrong number of followers returned

--- config/config2.js
+++ config/config2.js
@@ -1,5 +1,5 @@
 module.exports = {
-    PORT:process.env.PORT || '',
-    MONGO_URI:process.env.MONGO_URI || '',
-    JWT_KEY: process.env.JWT_KEY || ''
+    PORT:process.env.PORT || '<PORT>',
+    MONGO_URI:process.env.MONGO_URI || '<MONGO_URI>',
+    JWT_KEY: process.env.JWT_KEY || '<JWT_KEY>'
 }
